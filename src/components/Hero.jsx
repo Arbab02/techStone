2dd02620aff49cf75e11bf6928860841c1893536
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-03-09 19:24:00 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
     >
       <div className="container mx-auto flex  px-8 py-2 md:flex-row flex-col items-center">
         <motion.div
-          className="lg:flex-grow md:w-1/2 lg:pr-24  md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+          className="lg:flex-grow md:w-1/2 lg:pr-24  md:pr-16 flex flex-col md:items-start md:text-left mb-10 md:mb-0 items-center text-center"
           variants={containerVariants}
         >
           <motion.h1
