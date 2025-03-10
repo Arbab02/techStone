a7cdd7f88f7f435fa5e4aa85127125d814576565
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-03-10 11:55:04 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,12 +41,13 @@ const Hero = () => {
       className="  pt-[7rem] bg-white  body-font bg-gradient-to-tr from-[#000000] via-[#00071E] to-blue-950"
     >
       <div className="container mx-auto flex  px-8 py-2 md:flex-row flex-col items-center">
+        {/* mb-16 property was here below */}
         <motion.div
-          className="lg:flex-grow md:w-1/2 lg:pr-24  md:pr-16 flex flex-col md:items-start md:text-left mb-10 md:mb-0 items-center text-center"
+          className="lg:flex-grow md:w-1/2 lg:pr-24   md:pr-16 flex flex-col md:items-start md:text-left mb-10  md:mb-0 items-center text-center"
           variants={containerVariants}
         >
           <motion.h1
-            className="title-font sm:text-4xl lg:text-6xl text-4xl md:text-5xl mb-4 text-white font-bold"
+            className="title-font xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-4xl  mb-4 text-white font-bold"
             variants={childVariants}
           >
             Better Solutions For Your&nbsp;
@@ -54,21 +55,21 @@ const Hero = () => {
             <span className="text-blue-500  font-bold">Business!</span>
           </motion.h1>
           <motion.p
-            className="mb-8 leading-relaxed lg:text-2xl md:text-2xl text-xl text-center md:text-justify text-white"
+            className="mb-8 leading-relaxed xl:text-3xl lg:text-2xl md:text-xl text-xl text-center md:text-justify text-white"
             variants={childVariants}
           >
             We are team of talented designers making websites with ReactJS &
             TailwindCSS
             <a href="/addTopic">
-              <button className="block mx-auto lg:mx-0  xl:mx-0 md:mx-0 rounded-full text-xl bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 font-normal mt-4 text-white px-6 py-2 shadow-lg hover:from-purple-600 hover:via-pink-600 hover:to-pink-600 transition duration-300">
+              <button className="block mx-auto lg:mx-0  xl:mx-0 md:mx-0 rounded-full xl:text-xl lg:text-lg text-lg  bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 font-normal mt-4 text-white px-6 py-2  transition-all hover:scale-110 ">
                 Join Our Courses
               </button>
             </a>
           </motion.p>
-          <motion.div
+          {/* <motion.div
             className="flex justify-center"
             variants={childVariants}
-          ></motion.div>
+          ></motion.div> */}
         </motion.div>
         {/* pl-8 property was here below */}
         <motion.div
