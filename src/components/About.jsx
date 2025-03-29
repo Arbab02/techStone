df2e8ea9d8a3ddf2c0a7f711344368af62b09ca4
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-03-29 03:43:28 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,38 +13,36 @@ const About = () => {
         {/* Content */}
         <div className="font-sans flex flex-wrap items-center justify-center">
           <div className="w-full sm:w-11/12 md:w-3/4 lg:w-1/2 px-4 sm:px-6 lg:px-10 py-4">
-            <p className="text-gray-700 text-lg text-justify mb-5 leading-[1.5rem]">
+            <p className="text-gray-700 text-lg text-justify mb-5 leading-[1.5rem] ">
               Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
               irure dolor in reprehenderit in voluptate velit pariatur.
             </p>
-            <p className="flex items-start gap-2 text-gray-700 text-lg mt-2">
+            <p className="flex items-start gap-2 text-gray-700 text-lg">
               <IoCheckmarkDoneSharp className="text-blue-400 text-xl mt-1" />
               Ullamco laboris nisi ut aliquip ex ea commodo consequat
             </p>
-            <p className="flex items-start gap-2 text-gray-700 text-lg mt-2">
+            <p className="flex items-start gap-2 text-gray-700 text-lg ">
               <IoCheckmarkDoneSharp className="text-blue-400 text-xl mt-1" />
               Duis aute irure dolor in reprehenderit in voluptate velit
             </p>
-            <p className="flex items-start gap-2 text-gray-700 text-lg mt-2">
+            <p className="flex items-start gap-2 text-gray-700 text-lg ">
               <IoCheckmarkDoneSharp className="text-blue-400 text-xl mt-1" />
               Ullamco laboris nisi ut aliquip ex ea commodo consequat
             </p>
           </div>
           <div className="w-full sm:w-11/12 md:w-3/4 lg:w-1/2 px-4 sm:px-6 lg:px-10 py-4">
-            <p className="text-gray-700  text-lg text-justify mt-3 mb-5 leading-[1.5rem]">
+            <p className="text-gray-700  text-lg text-justify leading-[1.6rem] mt-2  ">
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
               officia deserunt mollit anim id est laborum. Duis aute irure dolor
               in reprehenderit. sunt in culpa qui officia deserunt mollit anim
-              id est laborum.sunt in culpa qui.officia deserunt mollit anim id
-              est laborum.sunt in culpa qui.
+              id est laborum.sunt in culpa qui.officia deserunt mollit anim.
             </p>
-            <div className="mt-1">
-              <a className="inline-flex items-center" href="#">
-                <button className="block mx-auto md:mx-0 rounded-full text-lg bg-transparent font-normal px-8 py-2 text-blue-500 border border-blue-500 transition-all hover:scale-105">
-                  Learn More
-                </button>
-              </a>
-            </div>
+
+            <a className="inline-flex items-center" href="#">
+              <button className="block mx-auto md:mx-0 rounded-full text-lg bg-transparent font-normal px-8 py-2 mt-4 text-blue-500 border border-blue-500 transition-all hover:scale-105">
+                Learn More
+              </button>
+            </a>
           </div>
         </div>
       </div>
