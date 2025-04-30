26ce95069e0a9c8bcb715a4829d0da332694bf64
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-04-29 23:13:24 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Loader from './components/Loader.jsx'; // 👈 Add this import
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import ImageSlider from './components/ImageSlider.jsx';
@@ -13,10 +14,22 @@ import FAQAccordion from './components/FAQAccordion.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
 import ScrollToTopButton from './components/ScrollToTopButton.jsx';
-// import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Simulate loading delay (e.g. data fetch or assets load)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -36,12 +49,10 @@ function App() {
       <div id="statistics">
         <Statistics />
       </div>
-      
+
       <div id="services">
         <Services />
       </div>
-
-      
 
       <div id="reviews">
         <TestimonialSlider />
@@ -66,6 +77,7 @@ function App() {
       <div id="contact">
         <Contact />
       </div>
+
       <Footer />
       <ScrollToTopButton />
     </>
